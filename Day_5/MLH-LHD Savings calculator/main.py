0097a249d8a41adaccf5e50b287f6e2c5fc34026
Sri-vishnu-r1431 <78591776+Sri-vishnu-r1431@users.noreply.github.com>
2021-10-15 10:54:49 +0530

    Add files via upload

---
+++ Day_5/MLH-LHD Savings calculator/main.py
@@ -0,0 +1,8 @@
+print("*******Savings Calculator*******\n\n")
+
+print("Enter your hourly income in Rs:\n")
+income = float(input())
+print("Your daily savings are:₹", income*6)
+print("Your weekly savings are:₹", income*7*6)
+print("Your monthly savings are:₹", income*7*6*30)
+print("Your yearly savings are:₹", income*7*6*30*12)
